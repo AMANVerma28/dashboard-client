41e4093ef5b2e91eb058f5c8fe8847fb5c92f699
AMANVerma28 <amansingh.v15@iiits.in>
2017-12-11 10:55:53 +0530

    Changes

--- analytics/views.py
+++ analytics/views.py
@@ -8,15 +8,7 @@ from django.http import Http404,HttpRequest
 # Create your views here.
 
 #View for Analytics Wells.
-#It requests data of just well model as it doesn't need others.
 def analyticswells(request):
-#    data = coreapi.Client()
-#    schema = data.get("http://127.0.0.1:2469/well/well")
-#    with open('static/json/analyticswell.json', 'w') as outfile:
-#        json.dump(schema, outfile)
-#    schema = data.get("http://127.0.0.1:2469/well/yield")
-#    with open('static/json/analyticsyield.json', 'w') as outfile:
-#        json.dump(schema, outfile)
     return render(request,'analytics/wells.html')
 
 def analyticswells1(request):
--- dashboard/views.py
+++ dashboard/views.py
@@ -7,66 +7,11 @@ from django.http import Http404,HttpRequest
 
 # Create your views here.
 
-#View for home page. Request to get the data from the server is sent.
-#When the data is recieved, it is stored in the static folder as a json file.
-#Home paga requests data of each model from server.
 def index(request):
-    """data = coreapi.Client()
-    schema = data.get("http://127.0.0.1:2469/household")
-    with open('static/json/household.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("http://127.0.0.1:2469/farm")
-    with open('static/json/farm.json', 'w') as outfile:
-	    json.dump(schema, outfile)
-    schema = data.get("http://127.0.0.1:2469/well")
-    with open('static/json/well.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("http://127.0.0.1:2469/member")
-    with open('static/json/member.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("http://127.0.0.1:2469/season")
-    with open('static/json/season.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("http://127.0.0.1:2469/crop")
-    with open('static/json/crop.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("http://127.0.0.1:2469/storage")
-    with open('static/json/storage.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("http://127.0.0.1:2469/yield")
-    with open('static/json/yield.json', 'w') as outfile:
-        json.dump(schema, outfile)"""
     return render(request,'dashboard/dashboard.html')   
 
-#View for charts.
-#It requests data of all models except yield as it doesn't need it.
 def showcharts(request):
-    """data = coreapi.Client()
-    schema = data.get("http://127.0.0.1:2469/household")
-    with open('static/json/household.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("http://127.0.0.1:2469/farm")
-    with open('static/json/farm.json', 'w') as outfile:
-	    json.dump(schema, outfile)
-    schema = data.get("http://127.0.0.1:2469/well")
-    with open('static/json/well.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("http://127.0.0.1:2469/season")
-    with open('static/json/season.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("http://127.0.0.1:2469/crop")
-    with open('static/json/crop.json', 'w') as outfile:
-        json.dump(schema, outfile)
-    schema = data.get("http://127.0.0.1:2469/storage")
-    with open('static/json/storage.json', 'w') as outfile:
-        json.dump(schema, outfile)"""
     return render(request,'dashboard/charts.html')
 
-#View for charts.
-#It requests data of just well model as it doesn't need others.
 def showwells(request):
-    """data = coreapi.Client()
-    schema = data.get("http://127.0.0.1:2469/well")
-    with open('static/json/well.json', 'w') as outfile:
-        json.dump(schema, outfile)"""
     return render(request,'dashboard/wells.html')
